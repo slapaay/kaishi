f9657771f0b67a0a56254b82e320af6047cf4909
Lachlan Slape <slapelachie@gmail.com>
2020-12-15 12:58:56 +1000

    change layout and prefs file

--- index.js
+++ index.js
@@ -1,131 +1,22 @@
-// ----- CONFIG -----
-
-let sites = {
-	"Social": {
-		"Reddit":			"https://reddit.com/",
-		"Twitter":			"https://twitter.com/",
-		"4chan":			"https://4chan.org/",
-		"Youtube":			"https://youtube.com/",
-	},		
-	"Dev": {
-		"GitHub":			"https://github.com/slapelachie/",
-		"Stack Overflow":	"https://stackoverflow.com/",
-	},
-};
-
-let quotes = [
-	{
-		"quote": "People only have substance within the meories of other people",
-		"author": "Lain Iwakura"
-	},
-	{
-		"quote": "If you fail you'll figure something else out. If you fail at that, at least you aren't dead - or dying any faster than the rest of us.",
-		"author": "7429538"
-	},
-	{
-		"quote": "...doesn't matter what drives your motivation, be it negative or positive emotions, as long as you do something. Motivation is all we need my friend",
-		"author": "7429111"
-	},
-	{
-		"quote": "Point is, negative, stigmatized emotions such as contempt, indignation, and anger could be used as a driving-force in positive development, if you allow it. Prove yourself wrong by breaking your own constraints.",
-		"author": "7429039"
-	},
-	{
-		"quote": "The world doesn't care how weak you are. Man up or die",
-		"author": "411062016"
-	},
-	{
-		"quote": "But you can be better you silly duck",
-		"author": "7222918"
-	},
-	{
-		"quote": "You must rebuild. You are not destroyed but merely damaged, gather your parts and data and re-assemble yourself in a greater image.",
-		"author": "621328591"
-	},
-	{
-		"quote": "If only you knew how different things could be",
-		"author": "45166513"
-	},
-	{
-		"quote": "People have to save themselves. One person saving another is impossible.",
-		"author": "Oshino Meme"
-	},
-	{
-		"quote": "It’s impossible for someone to replace someone else.",
-		"author": "Aragari Koyomi"
-	},
-	{
-		"quote": "People save themselves on their own. Nobody can ever save anyone else.",
-		"author": "Aragari Koyomi"
-	},
-	{
-		"quote": "The impostor is an impostor because it can’t be distinguished from the real thing. The proof of its falsity is in its resemblance to the real thing.",
-		"author": "Aragari Koyomi"
-	},
-	{
-		"quote": "Curiosity is like a cockroach. It likes to stumble upon secrets that are supposed to stay hidden.",
-		"author": "Hitagi Senjougahara"
-	},
-	{
-		"quote": "A fake with more power than the real thing is more dangerous than the real thing.",
-		"author": "Hitagi Senjougahara"
-	},
-	{
-		"quote": "The past is just the past. There’s no inherent value in getting over it or catching up to it.",
-		"author": "Kaiiki Daishu"
-	},
-	{
-		"quote": "Just as there isn’t a problem that can’t be solved by being right, there isn’t a problem that can’t be solved by money.",
-		"author": "Kaiiki Daishu"
-	},
-	{
-		"quote": "The fake is of far greater value. In its deliberate attempt to be real, it’s more real than the real thing.",
-		"author": "Kaiiki Daishu"
-	},
-	{
-		"quote": "Whatever you value… What you consider right is up to you. But don’t force those ideals onto other people.",
-		"author": "Kagenui Yozuru"
-	},
-	{
-		"quote": "You can lose the match and the game, but if you don’t lose to yourself, it isn’t a loss.",
-		"author": "Aragari Karen"
-	},
-	{
-		"quote": "Your heart will stop beating on the 2020-07-07T23:34Z",
-		"author": "Dez"
-	}
-];
-
-/*
- * Pads a value if it is less then 10 with a zero
- */
 function pad(value) {
 	return value < 10 ? '0' + value : value;
 }
 
-/*
- * Changes the quote on screen
- */
 function displayRandomQuote() {
 	let random_quote = quotes[Math.floor(Math.random()*quotes.length)];
 	document.getElementById("quote").innerHTML = `${random_quote.quote}`;
 	document.getElementById("author").innerHTML = `- ${random_quote.author}`;
 }
 
-/*
- *	Displays the clock on screen
- */
 function displayClock() {
+	days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
+	months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
+
     now = new Date(); 
-    time = `${pad(now.getHours())}:${pad(now.getMinutes())}`;
-	date = `${now.getFullYear()}-${pad(now.getMonth())}-${pad(now.getDate())}`
+	time = `${days[now.getDay()]}, ${months[now.getMonth()]} ${pad(now.getDate())} | ${pad(now.getHours())}:${pad(now.getMinutes())}`
     document.getElementById("clock_time").innerHTML = time;
-    document.getElementById("clock_date").innerHTML = date;
 }
 
-/*
- * Creates the link area in different categories
- */
 function populateLinkArea() {
 	link_html = "";	
 	for (let i in sites){
@@ -139,7 +30,7 @@ function populateLinkArea() {
 }
 
 async function init(){	
-	title = "kaishi";
+	title = "Start";
 
 	$(document).prop('title', title);
 }
---
+++ prefs.js
@@ -0,0 +1,148 @@
+// ----- BOOKMARKS -----
+
+let sites = {
+	"Weeb": {
+		"AniList": "https://anilist.co/",
+		"aniDB": "https://anidb.net",
+		"BakaBT": "https://bakabt.me/",
+		"LISTEN.moe": "https://listen.moe/",
+	},
+	"Social": {
+		"Reddit": "https://reddit.com/",
+		"Twitter": "https://twitter.com/",
+		"4chan": "https://4chan.org/",
+		"Youtube": "https://youtube.com/",
+	},
+	"Dev": {
+		"GitHub": "https://github.com/slapelachie/",
+		"ArchWiki": "https://wiki.archlinux.org/",
+		"AWS": "https://console.aws.amazon.com/console/home"
+	},
+};
+
+
+// ----- QUOTES -----
+
+let quotes = [
+	{
+		"quote": "If you fail you'll figure something else out. If you fail at that, at least you aren't dead - or dying any faster than the rest of us.",
+		"author": "7429538"
+	},
+	{
+		"quote": "...doesn't matter what drives your motivation, be it negative or positive emotions, as long as you do something. Motivation is all we need my friend",
+		"author": "7429111"
+	},
+	{
+		"quote": "Point is, negative, stigmatized emotions such as contempt, indignation, and anger could be used as a driving-force in positive development, if you allow it. Prove yourself wrong by breaking your own constraints.",
+		"author": "7429039"
+	},
+	{
+		"quote": "The world doesn't care how weak you are. Man up or die",
+		"author": "411062016"
+	},
+	{
+		"quote": "But you can be better you silly duck",
+		"author": "7222918"
+	},
+	{
+		"quote": "You must rebuild. You are not destroyed but merely damaged, gather your parts and data and re-assemble yourself in a greater image.",
+		"author": "621328591"
+	},
+	{
+		"quote": "If only you knew how different things could be",
+		"author": "45166513"
+	},
+	{
+		"quote": "People have to save themselves. One person saving another is impossible.",
+		"author": "Oshino Meme"
+	},
+	{
+		"quote": "People save themselves on their own. Nobody can ever save anyone else.",
+		"author": "Aragari Koyomi"
+	},
+	{
+		"quote": "The impostor is an impostor because it can’t be distinguished from the real thing. The proof of its falsity is in its resemblance to the real thing.",
+		"author": "Aragari Koyomi"
+	},
+	{
+		"quote": "Curiosity is like a cockroach. It likes to stumble upon secrets that are supposed to stay hidden.",
+		"author": "Hitagi Senjougahara"
+	},
+	{
+		"quote": "A fake with more power than the real thing is more dangerous than the real thing.",
+		"author": "Hitagi Senjougahara"
+	},
+	{
+		"quote": "The past is just the past. There’s no inherent value in getting over it or catching up to it.",
+		"author": "Kaiiki Daishu"
+	},
+	{
+		"quote": "Just as there isn’t a problem that can’t be solved by being right, there isn’t a problem that can’t be solved by money.",
+		"author": "Kaiiki Daishu"
+	},
+	{
+		"quote": "The fake is of far greater value. In its deliberate attempt to be real, it’s more real than the real thing.",
+		"author": "Kaiiki Daishu"
+	},
+	{
+		"quote": "Whatever you value… What you consider right is up to you. But don’t force those ideals onto other people.",
+		"author": "Kagenui Yozuru"
+	},
+	{
+		"quote": "You can lose the match and the game, but if you don’t lose to yourself, it isn’t a loss.",
+		"author": "Aragari Karen"
+	},
+	{
+		"quote": "The ordinary days that we live in may, in fact, be a series of miracles.",
+		"author": "Koujirou Sasahara"
+	},
+	{
+		"quote": "Don't let your regrets over the past trap you.",
+		"author": "Akihito Kanbara"
+	},
+	{
+		"quote": "If you keep trying to reason with yourself instead of listening to your feelings, you'll never be able to feel true happiness.",
+		"author": "Hiroomi Nase"
+	},
+	{
+		"quote": "What are you so hesitant about? It’s your dream, isn’t it? It’s right in front of you and you’re wavering? You gotta be reckless, and take whatever you can!",
+		"author": "Tomoya Okazaki"
+	},
+	{
+		"quote": "There's nothing in this world that is ever truly decided. Birds sometimes fall out of the sky, and sometimes it snows in April. Everything is uncertain, nothing is 'unnatural'.",
+		"author": "Kouhei Kadono"
+	},
+	{
+		"quote": "The brain is microcosm creating it's own stores. People then live out these stories",
+		"author": "Kirima Seiichi"
+	},
+	{
+		"quote": "Time does not exist, only the illusion of memories exist.",
+		"author": "Kirima Seiichi"
+	},
+	{
+		"quote": "That bird has no idea what he’s looking at, and yet, what does the bird do? Does he panic? No, he just does the best he can.",
+		"author": "Terry A. Davis"
+	},
+	{
+		"quote": "You wanna place yourself in a position to give maximum glory to your Creator. So you go out there, and you be somebody glorious, okay?",
+		"author": "Terry A. Davis"
+	},
+	{
+		"quote": "By making it a song, can I keep the memory？ I just came to love it now.",
+		"author": "Homecomings"
+	},
+	{
+		"quote": "I dreamt I was a butterfly. I couldn't tell I was dreaming. But when I woke, I was I and not a butterfly. Was I dreaming that I was the butterfly, or was the butterfly dreaming that it was me?",
+		"author": "Chuang-Tzu"
+	},
+	{
+		"quote": "Idiot! Stop doubting yourself!",
+		"author": "Artia"
+	},
+	{
+		"quote": "I wouldn't say you should work hard, but let's just try to live on together because we are not just by ourselves",
+		"author": "Watame"
+	}
+];
+
